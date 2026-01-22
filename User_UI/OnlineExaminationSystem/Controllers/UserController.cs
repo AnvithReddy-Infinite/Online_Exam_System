9a3c55860b2187202d2c7d020f5a184d74934d88
Pooja Tirmal <poojati@infinite.com>
2026-01-22 11:33:22 +0530

    UI folder structure

---
+++ User_UI/OnlineExaminationSystem/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineExaminationSystem.Controllers
+{
+    public class UserController : Controller
+    {
+    }
+}
---
+++ User_UI/OnlineExaminationSystem/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineExaminationSystem.Models
+{
+    public class UserModel
+    {
+    }
+}
