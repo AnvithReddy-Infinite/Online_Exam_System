29fbda9c10d9210ab70d8fc7e93358b74be62bff
waseetauqueera <waseeft@infinite.com>
2026-01-22 16:36:42 +0530

    Implementing user repository

--- OES_WepApi/Repository/IUserRepository.cs
+++ OES_WepApi/Repository/IUserRepository.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OES_WepApi.Models;
 
 namespace OES_WepApi.Repository
 {
     internal interface IUserRepository
     {
+        // Get a user by email (used for checking duplicates and login)
+        User GetUserByEmail(string email);
+
+        // Get a user by ID (optional, useful for reports)
+        User GetUserById(int userId);
+
+        // Add a new user (used for registration)
+        void AddUser(User user);
+
+        // Validate user credentials (for login)
+        bool ValidateUser(string email, string password);
+
+        // Get all users (for admin reports)
+        IEnumerable<User> GetAllUsers();
+
+
     }
 }
