eeefdc1dc9fe5e5d54f0c8ad483d18a756701358
AnvithReddy-Infinite <anvithr@infinite.com>
2026-01-22 17:45:22 +0530

    Uploading Admin UI

---
+++ User_UI/OnlineExaminationSystem/Controllers/AdminQuestionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineExaminationSystem.Controllers
+{
+    public class AdminQuestionController : Controller
+    {
+        // GET: AdminQuestion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ User_UI/OnlineExaminationSystem/Controllers/AdminTechnologyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineExaminationSystem.Controllers
+{
+    public class AdminTechnologyController : Controller
+    {
+        // GET: AdminTechnology
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AdminTechnology/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
